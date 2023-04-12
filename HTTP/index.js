e2638beef36e7c11cec0bdf715d9a827a9a138ea
Mridul <mit.ash.619@gmail.com>
2023-04-13 04:01:14 +0530

    update json response

--- HTTP/index.js
+++ HTTP/index.js
@@ -1,11 +1,12 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-  const obj = { name: 'Mridul', sex: 'male' };
+  //plain text
   //   res.writeHead(200, { 'Content-Type': 'text/plain' });
   //   res.end('Hello World!');
 
-  //sending JSON
+  //sending JSON response
+  const obj = { name: 'Mridul', sex: 'male' };
   res.writeHead(200, { 'Content-Type': 'application/json' });
   res.end(JSON.stringify(obj));
 });
