8b42205bca2ee414c76f7fe633c7baf4d9e2b264
Mridul <mit.ash.619@gmail.com>
2023-04-15 04:15:23 +0530

    html response from file

--- HTTP/index.js
+++ HTTP/index.js
@@ -1,4 +1,5 @@
 const http = require('http');
+const fs = require('fs');
 
 const server = http.createServer((req, res) => {
   //plain text
@@ -6,9 +7,18 @@ const server = http.createServer((req, res) => {
   //   res.end('Hello World!');
 
   //sending JSON response
-  const obj = { name: 'Mridul', sex: 'male' };
-  res.writeHead(200, { 'Content-Type': 'application/json' });
-  res.end(JSON.stringify(obj));
+  //   const obj = { name: 'Mridul', sex: 'male' };
+  //   res.writeHead(200, { 'Content-Type': 'application/json' });
+  //   res.end(JSON.stringify(obj));
+
+  // html response
+  //   res.writeHead(200, { 'Content-Type': 'text/html' });
+  //   res.end('<h1>Hello World!</h1>');
+
+  //html file
+  const html = fs.readFileSync('./index.html', 'utf-8');
+  res.writeHead(200, { 'Content-Type': 'text/html' });
+  res.end(html);
 });
 
 server.listen(3000, () => {
