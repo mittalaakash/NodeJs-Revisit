27373528c596191d2a79855086e1f533d65dee54
Mridul <mit.ash.619@gmail.com>
2023-05-05 02:05:38 +0530

    update buffer

--- Buffer.js
+++ Buffer.js
@@ -3,3 +3,8 @@ const buffer = Buffer.from('Apple');
 console.log(buffer);
 console.log(buffer.toString());
 console.log(buffer.toJSON());
+
+buffer.write('Ma');
+console.log(buffer);
+console.log(buffer.toString());
+console.log(buffer.toJSON());
