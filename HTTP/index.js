580f02e2a149e43447fde3b3630204eab7d776bb
Mridul <mit.ash.619@gmail.com>
2023-05-12 03:48:32 +0530

    create server

---
+++ HTTP/index.js
@@ -0,0 +1,10 @@
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.end('hello world!');
+});
+
+server.listen(3000, () => {
+  console.log('listening to port');
+});
