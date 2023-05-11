d0b1204daa1aa744b132b00d81e063521ff96b13
Mridul <mit.ash.619@gmail.com>
2023-05-11 23:54:38 +0530

    fs- promise module

---
+++ FS/fs-promise.js
@@ -0,0 +1,9 @@
+const fs = require('fs/promises');
+
+console.log('first');
+
+fs.readFile('./file.txt', 'utf-8')
+  .then(data => console.log(data))
+  .catch(err => console.log(err));
+
+console.log('second');
