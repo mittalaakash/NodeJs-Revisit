092c4a43f22573ab46953696debaace9f5eff3b9
Mridul <mit.ash.619@gmail.com>
2023-05-24 03:30:06 +0530

    CLI application

--- custom-cli/index.js
+++ custom-cli/index.js
@@ -1,3 +1,15 @@
 #!/usr/bin/env Node
+const yargs = require('yargs');
+const { argv } = yargs(process.argv);
 
-console.log('hello from CLI');
+const printMoves = async pokemonName => {
+  const response = await fetch(
+    `https://pokeapi.co/api/v2/pokemon/${pokemonName}`,
+  );
+
+  const pokemon = await response.json();
+  const moves = pokemon.moves.map(({ move }) => move.name);
+  console.log(moves);
+};
+
+printMoves('ditto');
