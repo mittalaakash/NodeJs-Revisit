d620b3942a51e1afbd9ba19b9a67a5d54f1a57ee
Mridul <mit.ash.619@gmail.com>
2023-05-24 04:36:54 +0530

    add prompt

--- custom-cli/index.js
+++ custom-cli/index.js
@@ -1,15 +1,31 @@
 #!/usr/bin/env Node
 const yargs = require('yargs');
+const inquirer = require('inquirer');
+
 const { argv } = yargs(process.argv);
 
 const printMoves = async pokemonName => {
-  const response = await fetch(
-    `https://pokeapi.co/api/v2/pokemon/${pokemonName}`,
-  );
+  try {
+    const response = await fetch(
+      `https://pokeapi.co/api/v2/pokemon/${pokemonName}`,
+    );
 
-  const pokemon = await response.json();
-  const moves = pokemon.moves.map(({ move }) => move.name);
-  console.log(moves);
+    const pokemon = await response.json();
+    const moves = pokemon.moves.map(({ move }) => move.name);
+    console.log(moves.slice(0, 5));
+  } catch (error) {
+    console.log(`error in fetching pokemon  with name ${pokemonName}`);
+  }
 };
 
-printMoves('ditto');
+const prompt = inquirer.createPromptModule();
+prompt([
+  {
+    type: 'input',
+    name: 'pokemon',
+    message: "Enter pokemon name to view it's moves: ",
+  },
+]).then(answer => {
+  const pokemon = answer.pokemon;
+  printMoves(pokemon);
+});
